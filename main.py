287d70ba89d7f7afaf30f6bd46ef44f575fe6bc1
And00re <and00re@mail.ru>
2018-07-07 00:53:13 +0500

    Parse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import bokeh
-
-a = 10
-
-print(a)
+import xlrd
+import pandas as pd
+import numpy
+'''
+book = xlrd.open_workbook('09.03.02 ИСиТ О Б.XLS')
+sheet = book.sheet_by_index(0)
+for row in range(5, sheet.nrows):
+    print(sheet.row_values(row))
+'''
+sheet = pd.read_excel('09.03.02 ИСиТ О Б.XLS',skiprows=5,index_col=0)
+print(sheet)
