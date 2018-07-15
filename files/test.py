4f61d50c6e47fab82d5087099dde677b0384308d
And00re <and00re@mail.ru>
2018-07-16 02:23:31 +0500

    Attempt to merge

--- files/test.py
+++ files/test.py
@@ -1,60 +1,62 @@
 import pandas as pd
-import numpy
+import numpy as np
 import os
+from os.path import dirname, join 
 
 from bokeh.layouts import widgetbox, row
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.widgets import DataTable, TableColumn, CheckboxGroup, Button
 from bokeh.io import curdoc
 
-files = os.listdir('./docs')
+files = os.listdir(join(dirname(__file__), '../docs'))
 directions = []
 for file in range(len(files)):
     directions.append([files[file].split()[i] for i in range(len(files[file].split()))])
     directions[file][3] = directions[file][3].replace('.XLS', '')
 
-df = pd.read_excel('./docs/' + files[0], skiprows=5, index_col=0, usecols=[0,1,3,4,5,6,9]) 
-source = ColumnDataSource(data = dict())
-
-def make_Table(df):
-    source = ColumnDataSource(df)
-    columns = [TableColumn(field=df.columns.values[column], title=df.columns.values[column]) for column in range(df.shape[1])]
-    return DataTable(source=source, columns=columns)
-
 def update(attr, old, new):
     #act = [checkbox_group.labels[i] for i in checkbox_group.active]
+    df_new = df_initial
     for i in checkbox_group.active:
-       df = pd.read_excel('./docs/' + files[i], skiprows=5, index_col=0, usecols=[0,1,3,4,5,6,9]) 
-    source.data = {df.columns.values[column] : df[df.columns.values[column]] for column in range(df.shape[1])}
-
-#def form():
-#   source = ColumnDataSource(df)
-#   data_table = DataTable(source=source, columns=columns)
+        df = pd.read_excel(join(dirname(__file__), '../docs', files[i]), skiprows=5, index_col=0, usecols=[0,1,3,4,5,6,9])
+        df[df.columns.values[5]] = df[df.columns.values[5]].fillna('')
+        for line in df[df.columns.values[5]].index:
+            if df.loc[line,df.columns.values[5]] != '':
+                df.loc[line, df.columns.values[5]] = directions[i][1]
+        df_new = pd.merge(df_new, df, how='outer', on=df.columns.values.tolist())
+        #df_new.join(df.set_index(df.columns.values[0]),on=df.columns.values[0])
+        #df_new = pd.concat([df,df_new])
+        #df_new.groupby(df.columns.values[1])
+        #print(df[df.columns.values[5]])
+    #print(df_new)
+    source.data = {df_new.columns.values[column] : df_new[df_new.columns.values[column]] for column in range(df_new.shape[1])}
 
 labels = [directions[i][1] for i in range(len(directions))]
 checkbox_group = CheckboxGroup(labels=labels)
 checkbox_group.on_change('active', update)
 
-#button = Button(label='Сформировать список', button_type = "success")
-#button.on_click(update)
-#button.callback = CustomJS(args=dict(active=act),code=open('./updateTable.js').read())
-#initial_carriers = [checkbox_group.labels[i] for i in checkbox_group.active]
+df_initial = pd.read_excel(join(dirname(__file__), '../docs', files[0]), skiprows=5, index_col=0, usecols=[0,1,3,4,5,6,9], nrows=0) 
+source = ColumnDataSource(data = dict())
+columns = [TableColumn(field=df_initial.columns.values[column], title=df_initial.columns.values[column]) for column in range(df_initial.shape[1])]
+data_table = DataTable(source=source, columns=columns, width=1000, height=2000)
 
-columns = [TableColumn(field=df.columns.values[column], title=df.columns.values[column]) for column in range(df.shape[1])]
-data_table = DataTable(source=source, columns=columns, width=800)
-
-#init DataTable
-#initial_carriers = checkbox_group.labels[i] for i in checkbox_group.active]
-#df1 = pd.read_excel('./docs/' + files[4], skiprows=5, index_col=0)
-
-#df2 = pd.read_excel('./docs/' + files[3], skiprows=5, index_col=0)
-
-#df3  = df2.merge(df1, how='outer')
-
+'''
+df_new = df_initial
+for i in range(2):
+    df = pd.read_excel(join(dirname(__file__), '../docs', files[i]), skiprows=5, index_col=0, usecols=[0,1,3,4,5,6,9])
+    df[df.columns.values[5]] = df[df.columns.values[5]].fillna('')
+    for line in df[df.columns.values[5]].index:
+        if df.loc[line,df.columns.values[5]] != '':
+            df.loc[line, df.columns.values[5]] = directions[i][1]
+    df_new = pd.merge(left=df_new, right=df, how='right', on=df.columns.values.tolist(), validate="one_to_one")
+    #df_new = pd.concat([df,df_new])
+    #df_new.groupby(df.columns.values[6])
+    #print(df[df.columns.values[5]])
+print(df_new)
+'''
 control = widgetbox(checkbox_group)
 table = widgetbox(data_table)
 
 curdoc().add_root(row(control,table))
 
 #bokeh serve --show files\test.py
-update
