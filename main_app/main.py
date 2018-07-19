dae1952f454e58a340f684732acbd938edfbb7a3
And00re <and00re@mail.ru>
2018-07-20 02:39:58 +0500

    Working sorting

--- main_app/main.py
+++ main_app/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import os
-from os.path import dirname, join 
+from os.path import dirname, join
+import collections 
 
 from bokeh.layouts import widgetbox, row, column, layout
 from bokeh.models import ColumnDataSource, CustomJS
@@ -15,8 +16,27 @@ for file in range(len(files)):
     directions[file][-1] = directions[file][-1].replace('.XLS', '')
     
 def update(attr, old, new):
+
+    def sort(attr, old, new):
+        did = False
+        res = []
+        temp = []
+        for i in checkbox_button_group.active:
+            for line in range(df_new.shape[0]):
+                #print(df_new[df_new.columns.values[4]][line])  
+                if button_labels[i] == df_new[df_new.columns.values[4]][line]:
+                    temp.append(line)
+                    did = True
+                if button_labels[i] == df_new[df_new.columns.values[5]][line]:
+                    temp.append(line)
+                    did = True
+
+        if did == True:
+            res = [item for item in df_new.index.values.tolist() if item not in temp]
+        df_new_new = df_new.drop(res).reset_index(drop=True)
+        source.data = {df_new_new.columns.values[column] : df_new_new[df_new_new.columns.values[column]] for column in range(df_new_new.shape[1])}
+
     df_new = df_initial
-    
     for i in checkbox_group.active:
         df = pd.read_excel(join(dirname(__file__), 'docs', files[i]), skiprows=5, usecols=[0,1,3,4,5,6,9])
         df[df.columns.values[6]] = df[df.columns.values[6]].fillna('')
@@ -40,9 +60,6 @@ def update(attr, old, new):
     
     source.data = {df_new.columns.values[column] : df_new[df_new.columns.values[column]] for column in range(df_new.shape[1])}
 
-def sort(attr, old, new):
-        div = Div(text = str(new))
-        curdoc().add_root(div)
 
 labels = [' '.join(directions[i]) for i in range(len(directions))]
 checkbox_group = CheckboxGroup(labels=labels)
@@ -68,8 +85,8 @@ for i in range(5):
 df_new = df_new.drop(df_new.columns.values[0], axis=1).reset_index(drop=True)
 button_labels = pd.concat([df_new[df_new.columns.values[4]], df_new[df_new.columns.values[5]]]).unique().tolist()
 del button_labels[button_labels.index('')]
+print(df_new.index.values.tolist())
 '''
-
 control = widgetbox(checkbox_group)
 table = widgetbox(data_table)
 l = layout(children=[widgetbox(CheckboxButtonGroup()), widgetbox(DataTable())])
